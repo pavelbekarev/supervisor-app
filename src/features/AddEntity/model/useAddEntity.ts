78f776c8f1c9a68610147a89eb1bf9d285b53a49
pavelbekarev <bekarev.ps@gmail.com>
2026-07-05 18:20:50 +0500

    feat: add todo validation

--- src/features/AddEntity/model/useAddEntity.ts
+++ src/features/AddEntity/model/useAddEntity.ts
@@ -1,7 +1,9 @@
 import type { Todo } from "#entities/Todo";
 import { useTodoList } from "#entities/Todo/model/store";
+import { TodoValidation } from "#entities/Todo/model/validation";
 import { useModalStore } from "#shared/ui/Modal/model/store";
 import { useState } from "react";
+import * as Yup from "yup";
 
 export function useAddEntity() {
   const [formData, setFormData] = useState<Todo>({
@@ -13,11 +15,19 @@ export function useAddEntity() {
   const addTodo = useTodoList((state) => state.addTodo);
   const close = useModalStore((state) => state.close);
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    addTodo(formData);
-    close();
+    try {
+      await TodoValidation.validate(formData);
+      addTodo(formData);
+      close();
+    } catch (e) {
+      if (e instanceof Yup.ValidationError) {
+        alert(e.message);
+        return;
+      }
+    }
   };
 
   const handleChange = (e: any) => {
--- src/features/EditEntity/model/useEditEntity.ts
+++ src/features/EditEntity/model/useEditEntity.ts
@@ -1,18 +1,28 @@
 import type { Todo } from "#entities/Todo";
 import { useTodoList } from "#entities/Todo/model/store";
+import { TodoValidation } from "#entities/Todo/model/validation";
 import { useModalStore } from "#shared/ui/Modal/model/store";
 import { useState } from "react";
+import * as Yup from "yup";
 
 export function useEditEntity(editData: Todo) {
   const [formData, setFormData] = useState<Todo>(editData);
   const { updateTodo } = useTodoList();
   const close = useModalStore((state) => state.close);
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    updateTodo(editData.id, formData);
-    close();
+    try {
+      await TodoValidation.validate(formData);
+      updateTodo(editData.id, formData);
+      close();
+    } catch (e) {
+      if (e instanceof Yup.ValidationError) {
+        alert(e.message);
+        return;
+      }
+    }
   };
 
   const handleChange = (e: any) => {
